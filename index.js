0bd45e3072e14b6d8414cd03c5f8d6d23b20a5fe
Simen Li <simenkid@gmail.com>
2016-09-12 22:14:30 +0800

    readme.md

--- index.js
+++ index.js
@@ -2,31 +2,31 @@ var proving = {};
 
 proving.string = function (val, msg) {
     if (typeof val !== 'string')
-        throw new TypeError(msg || 'input value should be a string.');
+        throw new TypeError(msg || 'Input value should be a string.');
     return true;
 };
 
 proving.number = function (val, msg) {
     if (typeof val !== 'number' || isNaN(val))
-        throw new TypeError(msg || 'input value should be a number and cannot be a NaN.');
+        throw new TypeError(msg || 'Input value should be a number and cannot be a NaN.');
     return true;
 };
 
 proving.boolean = function (val, msg) {
     if (typeof val !== 'boolean')
-        throw new TypeError(msg || 'input value should be a bool.');
+        throw new TypeError(msg || 'Input value should be a bool.');
     return true;
 };
 
 proving.array = function (val, msg) {
     if (!Array.isArray(val))
-        throw new TypeError(msg || 'input value should be an array.');
+        throw new TypeError(msg || 'Input value should be an array.');
     return true;
 };
 
 proving.fn = function (val, msg) {
     if (typeof val !== 'function')
-        throw new TypeError(msg || 'input value should be a function.');
+        throw new TypeError(msg || 'Input value should be a function.');
     return true;
 }
 
@@ -38,13 +38,12 @@ proving.object = function (val, msg) {
 
 proving.defined = function (val, msg) {
     if (undefined === val)
-        throw new TypeError(msg || 'Value should be given.');
+        throw new TypeError(msg || 'Input value should be given.');
     return true;
 };
 
 proving.stringOrNumber = function (val, msg) {
     msg = msg || 'Input value should be a number or a string.';
-
     if (typeof val === 'number') {
         if (isNaN(val))
             throw new TypeError(msg);
